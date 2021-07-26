a1e3871442c73270dbef30b0d986be8cec985513
Santa claus <santa@nortpole.com>
2021-07-26 08:14:10 +0300

    add views fucntion my profile

--- neighbour/views.py
+++ neighbour/views.py
@@ -36,12 +36,12 @@ def registration(request):
     return render(request, 'registration/register.html', context)
 
 
-# @login_required(login_url='/accounts/login/')
-# def my_profile(request):
-#     current_user=request.user
-#     profile =Profile.objects.get(username=current_user)
+@login_required(login_url='/accounts/login/')
+def my_profile(request):
+    current_user=request.user
+    profile =Profile.objects.get(username=current_user)
 
-#     return render(request,'user_profile.html',{"profile":profile})
+    return render(request,'user_profile.html',{"profile":profile})
 
 
 # @login_required(login_url='/accounts/login/')
