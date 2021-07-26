1db07cb7d1f80a5cfe04876a679e1d4da3dffdef
Santa claus <santa@nortpole.com>
2021-07-26 08:14:58 +0300

    add views fucntion create profile

--- neighbour/views.py
+++ neighbour/views.py
@@ -49,23 +49,23 @@ def user_profile(request,username):
     user = User.objects.get(username=username)
     profile =Profile.objects.get(username=user)
 
-#     return render(request,'user_profile.html',{"profile":profile})
+    return render(request,'user_profile.html',{"profile":profile})
 
-# @login_required(login_url='/accounts/login/')
-# def create_profile(request):
-#     current_user=request.user
-#     if request.method=="POST":
-#         form =ProfileForm(request.POST,request.FILES)
-#         if form.is_valid():
-#             profile = form.save(commit = False)
-#             profile.username = current_user
-#             profile.save()
-#         return HttpResponseRedirect('/')
+@login_required(login_url='/accounts/login/')
+def create_profile(request):
+    current_user=request.user
+    if request.method=="POST":
+        form =ProfileForm(request.POST,request.FILES)
+        if form.is_valid():
+            profile = form.save(commit = False)
+            profile.username = current_user
+            profile.save()
+        return HttpResponseRedirect('/')
 
-#     else:
+    else:
 
-#         form = ProfileForm()
-#     return render(request,'profile_form.html',{"form":form})  
+        form = ProfileForm()
+    return render(request,'profile_form.html',{"form":form})  
 
 
 
