ce05f9997a4c9d4d747ce2f3be7a0f2173add7ad
Santa claus <santa@nortpole.com>
2021-07-26 08:12:44 +0300

    add views index

--- neighbour/views.py
+++ neighbour/views.py
@@ -21,132 +21,132 @@ def index(request):
 
     return render(request,'index.html')
 
-def registration(request):
-    if request.method == 'POST':
-        form = RegisterForm(request.POST)
-        if form.is_valid():
-            username = form.cleaned_data.get('username')
-            form.save()
-            return redirect('login')
-    else:
-        form = RegisterForm()
-    context = {
-        'form':form,
-    }
-    return render(request, 'registration/register.html', context)
+# def registration(request):
+#     if request.method == 'POST':
+#         form = RegisterForm(request.POST)
+#         if form.is_valid():
+#             username = form.cleaned_data.get('username')
+#             form.save()
+#             return redirect('login')
+#     else:
+#         form = RegisterForm()
+#     context = {
+#         'form':form,
+#     }
+#     return render(request, 'registration/register.html', context)
 
 
-@login_required(login_url='/accounts/login/')
-def my_profile(request):
-    current_user=request.user
-    profile =Profile.objects.get(username=current_user)
+# @login_required(login_url='/accounts/login/')
+# def my_profile(request):
+#     current_user=request.user
+#     profile =Profile.objects.get(username=current_user)
 
-    return render(request,'user_profile.html',{"profile":profile})
-
-
-@login_required(login_url='/accounts/login/')
-def user_profile(request,username):
-    user = User.objects.get(username=username)
-    profile =Profile.objects.get(username=user)
-
-    return render(request,'user_profile.html',{"profile":profile})
-
-@login_required(login_url='/accounts/login/')
-def create_profile(request):
-    current_user=request.user
-    if request.method=="POST":
-        form =ProfileForm(request.POST,request.FILES)
-        if form.is_valid():
-            profile = form.save(commit = False)
-            profile.username = current_user
-            profile.save()
-        return HttpResponseRedirect('/')
-
-    else:
-
-        form = ProfileForm()
-    return render(request,'profile_form.html',{"form":form})  
+#     return render(request,'user_profile.html',{"profile":profile})
 
 
+# @login_required(login_url='/accounts/login/')
+# def user_profile(request,username):
+#     user = User.objects.get(username=username)
+#     profile =Profile.objects.get(username=user)
 
-@login_required(login_url='/accounts/login/')
-def update_profile(request):
-    current_user=request.user
-    if request.method=="POST":
-        instance = Profile.objects.get(username=current_user)
-        form =ProfileForm(request.POST,request.FILES,instance=instance)
-        if form.is_valid():
-            profile = form.save(commit = False)
-            profile.username = current_user
-            profile.save()
+#     return render(request,'user_profile.html',{"profile":profile})
 
-        return redirect('index')
+# @login_required(login_url='/accounts/login/')
+# def create_profile(request):
+#     current_user=request.user
+#     if request.method=="POST":
+#         form =ProfileForm(request.POST,request.FILES)
+#         if form.is_valid():
+#             profile = form.save(commit = False)
+#             profile.username = current_user
+#             profile.save()
+#         return HttpResponseRedirect('/')
 
-    elif Profile.objects.get(username=current_user):
-        profile = Profile.objects.get(username=current_user)
-        form = ProfileForm(instance=profile)
-    else:
-        form = ProfileForm()
+#     else:
 
-    return render(request,'update_profile.html',{"form":form})  
-
-@login_required(login_url='/accounts/login/')
-def authorities(request):
-    current_user=request.user
-    profile=Profile.objects.get(username=current_user)
-    authorities = Authorities.objects.filter(neighbourhood=profile.neighbourhood)
-
-    return render(request,'security.html',{"authorities":authorities})   
+#         form = ProfileForm()
+#     return render(request,'profile_form.html',{"form":form})  
 
 
-@login_required(login_url='/accounts/login/')
-def health(request):
-    current_user=request.user
-    profile=Profile.objects.get(username=current_user)
-    health = Health.objects.filter(neighbourhood=profile.neighbourhood)
 
-    return render(request,'health.html',{"health":health})     
+# @login_required(login_url='/accounts/login/')
+# def update_profile(request):
+#     current_user=request.user
+#     if request.method=="POST":
+#         instance = Profile.objects.get(username=current_user)
+#         form =ProfileForm(request.POST,request.FILES,instance=instance)
+#         if form.is_valid():
+#             profile = form.save(commit = False)
+#             profile.username = current_user
+#             profile.save()
 
-@login_required(login_url='/accounts/login/')
-def post(request):
-    current_user=request.user
-    profile=Profile.objects.get(username=current_user)
-    posts = Post.objects.filter(neighbourhood=profile.neighbourhood)
+#         return redirect('index')
 
-    return render(request,'post.html',{"posts":posts})      
+#     elif Profile.objects.get(username=current_user):
+#         profile = Profile.objects.get(username=current_user)
+#         form = ProfileForm(instance=profile)
+#     else:
+#         form = ProfileForm()
 
-@login_required(login_url='/accounts/login/')
-def new_post(request):
-    current_user=request.user
-    profile =Profile.objects.get(username=current_user)
+#     return render(request,'update_profile.html',{"form":form})  
 
-    if request.method=="POST":
-        form = PostForm(request.POST,request.FILES)
-        if form.is_valid():
-            post = form.save(commit = False)
-            post.username = current_user
-            post.neighbourhood = profile.neighbourhood
-            post.avatar = profile.avatar
-            post.save()
+# @login_required(login_url='/accounts/login/')
+# def authorities(request):
+#     current_user=request.user
+#     profile=Profile.objects.get(username=current_user)
+#     authorities = Authorities.objects.filter(neighbourhood=profile.neighbourhood)
 
-        return HttpResponseRedirect('/post')
+#     return render(request,'security.html',{"authorities":authorities})   
 
-    else:
-        form = PostForm()
 
-    return render(request,'post_form.html',{"form":form})    
+# @login_required(login_url='/accounts/login/')
+# def health(request):
+#     current_user=request.user
+#     profile=Profile.objects.get(username=current_user)
+#     health = Health.objects.filter(neighbourhood=profile.neighbourhood)
+
+#     return render(request,'health.html',{"health":health})     
+
+# @login_required(login_url='/accounts/login/')
+# def post(request):
+#     current_user=request.user
+#     profile=Profile.objects.get(username=current_user)
+#     posts = Post.objects.filter(neighbourhood=profile.neighbourhood)
+
+#     return render(request,'post.html',{"posts":posts})      
+
+# @login_required(login_url='/accounts/login/')
+# def new_post(request):
+#     current_user=request.user
+#     profile =Profile.objects.get(username=current_user)
+
+#     if request.method=="POST":
+#         form = PostForm(request.POST,request.FILES)
+#         if form.is_valid():
+#             post = form.save(commit = False)
+#             post.username = current_user
+#             post.neighbourhood = profile.neighbourhood
+#             post.avatar = profile.avatar
+#             post.save()
+
+#         return HttpResponseRedirect('/post')
+
+#     else:
+#         form = PostForm()
+
+#     return render(request,'post_form.html',{"form":form})    
     
-@login_required(login_url='/accounts/login/')
-def search_results(request):
-    if 'post' in request.GET and request.GET["post"]:
-        search_term = request.GET.get("post")
-        searched_posts = Post.search_post(search_term)
-        message=f"{search_term}"
+# @login_required(login_url='/accounts/login/')
+# def search_results(request):
+#     if 'post' in request.GET and request.GET["post"]:
+#         search_term = request.GET.get("post")
+#         searched_posts = Post.search_post(search_term)
+#         message=f"{search_term}"
 
-        print(searched_posts)
+#         print(searched_posts)
 
-        return render(request,'search.html',{"message":message,"posts":searched_posts})
+#         return render(request,'search.html',{"message":message,"posts":searched_posts})
 
-    else:
-        message="You haven't searched for any term"
-        return render(request,'search.html',{"message":message})    
+#     else:
+#         message="You haven't searched for any term"
+#         return render(request,'search.html',{"message":message})    
