a3189ff47247078e66fbb5fb8ff0bdbc3e9aac52
Santa claus <santa@nortpole.com>
2021-07-26 08:06:03 +0300

    add model class neighbourhood

--- neighbour/models.py
+++ neighbour/models.py
@@ -21,53 +21,53 @@ class neighbourhood(models.Model):
     def delete_neighbourhood(cls,neighbourhood):
         cls.objects.filter(neighbourhood=neighbourhood).delete()
 
-class Profile(models.Model):
-    avatar = models.ImageField(upload_to='avatars/')
-    description = HTMLField()
-    neighbourhood = models.ForeignKey(neighbourhood,on_delete=models.CASCADE)
-    username = models.ForeignKey(User,on_delete=models.CASCADE)
-    name =models.CharField(max_length=100)
-    email = models.EmailField()
+# class Profile(models.Model):
+#     avatar = models.ImageField(upload_to='avatars/')
+#     description = HTMLField()
+#     neighbourhood = models.ForeignKey(neighbourhood,on_delete=models.CASCADE)
+#     username = models.ForeignKey(User,on_delete=models.CASCADE)
+#     name =models.CharField(max_length=100)
+#     email = models.EmailField()
 
-    def __str__(self):
-        return self.name
+#     def __str__(self):
+#         return self.name
     
-class Authorities(models.Model):
-    neighbourhood = models.ForeignKey(neighbourhood,on_delete=models.CASCADE)
-    name =models.CharField(max_length=100)
-    email = models.EmailField()
-    contact = models.IntegerField()
-    address =models.CharField(max_length=100)
+# class Authorities(models.Model):
+#     neighbourhood = models.ForeignKey(neighbourhood,on_delete=models.CASCADE)
+#     name =models.CharField(max_length=100)
+#     email = models.EmailField()
+#     contact = models.IntegerField()
+#     address =models.CharField(max_length=100)
 
-    def __str__(self):
-        return self.name      
+#     def __str__(self):
+#         return self.name      
        
        
-class Health(models.Model):
-    logo = models.ImageField(upload_to='healthlogo/')
-    neighbourhood = models.ForeignKey(neighbourhood,on_delete=models.CASCADE)
-    name =models.CharField(max_length=100)
-    email = models.EmailField()
-    contact = models.IntegerField()
-    address =models.CharField(max_length=100)
+# class Health(models.Model):
+#     logo = models.ImageField(upload_to='healthlogo/')
+#     neighbourhood = models.ForeignKey(neighbourhood,on_delete=models.CASCADE)
+#     name =models.CharField(max_length=100)
+#     email = models.EmailField()
+#     contact = models.IntegerField()
+#     address =models.CharField(max_length=100)
 
 
-    def __str__(self):
-        return self.name    
+#     def __str__(self):
+#         return self.name    
 
-class Post(models.Model):
-    title = models.CharField(max_length=150)
-    image = models.ImageField(upload_to='post/')
-    post = HTMLField()
-    username = models.ForeignKey(User,on_delete=models.CASCADE)
-    neighbourhood= models.ForeignKey(neighbourhood,on_delete=models.CASCADE)
-    post_date = models.DateTimeField(auto_now_add=True)
-    avatar = models.ImageField(upload_to='avatars/')
+# class Post(models.Model):
+#     title = models.CharField(max_length=150)
+#     image = models.ImageField(upload_to='post/')
+#     post = HTMLField()
+#     username = models.ForeignKey(User,on_delete=models.CASCADE)
+#     neighbourhood= models.ForeignKey(neighbourhood,on_delete=models.CASCADE)
+#     post_date = models.DateTimeField(auto_now_add=True)
+#     avatar = models.ImageField(upload_to='avatars/')
 
-    def __str__(self):
-        return self.title    
+#     def __str__(self):
+#         return self.title    
 
-    @classmethod
-    def search_post(cls,search_term):
-        posts = cls.objects.filter(Q(username__username=search_term) | Q(neighbourhood__neighbourhood=search_term) | Q(title__icontains=search_term))
-        return posts              
+#     @classmethod
+#     def search_post(cls,search_term):
+#         posts = cls.objects.filter(Q(username__username=search_term) | Q(neighbourhood__neighbourhood=search_term) | Q(title__icontains=search_term))
+#         return posts              
